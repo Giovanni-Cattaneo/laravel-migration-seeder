0c0efde455ec5079d5f98ffd61d01136f7b037d5
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-10 18:04:08 +0200

    add link for contacts

--- resources/views/guest/contacts.blade.php
+++ resources/views/guest/contacts.blade.php
@@ -1,5 +1,18 @@
 @extends('layout.app')
 
 @section('main-content')
-    <h1>Contatti</h1>
+    <div class=" container p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Contatti</h1>
+            <p class="col-md-8 fs-4">
+                Benvenuto nella sezione contatti di Trenitalia, qui potrai ricevere notifhce dei tuoi viaggi e ricevere
+                assistenza per eventuali disguidi o malfunzionamenti vari
+            </p>
+            <a href="https://www.youtube.com/shorts/41iWg91yFv0"><button class="btn btn-primary btn-lg" type="button">
+                    Contattaci
+                </button></a>
+        </div>
+    </div>
+    <p class="text-center">Per altri reclami o ricevere un risarcimento clicca <a
+            href="https://www.youtube.com/watch?v=H6M1OF_E0IA">qui</a></p>
 @endsection
